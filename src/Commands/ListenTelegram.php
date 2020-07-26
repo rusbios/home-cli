8919adaf512a017629ae7736e52a04310600063e
Ryssia <ru.rasulov@petrovich.ru>
2020-07-26 12:45:22 +0300

    add telegramm access cli

--- src/Commands/ListenTelegram.php
+++ src/Commands/ListenTelegram.php
@@ -4,10 +4,12 @@ declare(strict_types = 1);
 namespace HomeCli\Commands;
 
 use HomeCli\Config;
+use HomeCli\Services\ChatBot;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use TelegramBot\Api\BotApi;
 
 class ListenTelegram extends CommandAbstract
 {
@@ -17,6 +19,7 @@ class ListenTelegram extends CommandAbstract
     {
         $this->setDescription('Слушает команды из телеграмм канала и выполняет их');
         $this->addOption('token', 't', InputOption::VALUE_OPTIONAL, 'Токен для подлючения к телеграму');
+        $this->addOption('password', 'p', InputOption::VALUE_OPTIONAL, 'Пароль для авторизации пользователя в телеграмме');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -28,9 +31,44 @@ class ListenTelegram extends CommandAbstract
             $config->save();
         }
 
+        if ($input->getOption('password')) {
+            $config->set('telegram.accessPas', $input->getOption('password'));
+            $config->save();
+        }
+
         if (!$config->get('telegram.token')) {
             $output->writeln('Нет токена для подключения');
             return Command::FAILURE;
+        }
+
+        $telegramBot = new BotApi($config->get('telegram.token'));
+        $chatBot = new ChatBot($telegramBot, $config);
+
+        $last = $config->get('telegram.update.last', 0);
+
+        while (true) {
+            try {
+                foreach ($telegramBot->getUpdates($last) as $update) {
+                    $message = $update->getMessage() ?? $update->getEditedMessage();
+                    $output->writeln('send message: ' . $message->getText());
+                    $answerMessage = $chatBot->run(
+                        $message->getText(),
+                        $message->getChat()->getUsername()
+                    );
+                    $output->writeln('answer message: ' . $answerMessage);
+                    $telegramBot->sendMessage(
+                        $message->getChat()->getId(),
+                        $answerMessage
+                    );
+
+                    $last = $update->getUpdateId() + 1;
+                }
+            } catch (\Exception $e) {
+                $output->writeln($e->getMessage());
+            }
+
+            $config->set('telegram.update.last', $last);
+            $config->save();
         }
 
         return parent::execute($input, $output);
---
+++ src/Services/ChatBot.php
@@ -0,0 +1,136 @@
+<?php
+declare(strict_types = 1);
+
+namespace HomeCli\Services;
+
+use HomeCli\Config;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Exception;
+use TelegramBot\Api\InvalidArgumentException;
+
+class ChatBot
+{
+    private const COMMAND_STAT = 'stat';
+    private const COMMAND_LIST = 'list';
+    private const COMMAND_ACCESS = 'access';
+    private const COMMAND_CLI = 'cli';
+    private const COMMAND_REG = 'reg';
+    private const COMMAND_BOT = 'bot';
+    private const COMMAND_ACCESS_CLEAR = 'access-clear';
+
+    private const COMMANDS = [
+        self::COMMAND_ACCESS => 'Список авторизованых пользователей',
+        self::COMMAND_CLI => 'Выполнить консольную команду на сервере',
+        self::COMMAND_LIST => 'Вывод списка доступных команд',
+        self::COMMAND_STAT => 'Какаято статистика',
+        self::COMMAND_BOT => 'Выполнить команду локального бота',
+        self::COMMAND_ACCESS_CLEAR => 'Разавторизовать всех',
+    ];
+
+    private array $history;
+
+    private BotApi $botApi;
+    private Config $config;
+    private string $accessPas;
+    private array $accessLogins;
+    private string $glue = '
+';
+
+    /**
+     * ChatBot constructor.
+     * @param BotApi $botApi
+     * @param Config $config
+     */
+    public function __construct(BotApi $botApi, Config $config)
+    {
+        $this->botApi = $botApi;
+        $this->accessPas = (string)$config->get('telegram.accessPas', 1);
+        $this->accessLogins = (array)$config->get('telegram.accessUsers', []);
+        $this->config = $config;
+    }
+
+    /**
+     * @param string $message
+     * @param string $userName
+     * @return string
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    public function run(string $message, string $userName): string
+    {
+        if (substr($message, 0, 1) != '/') {
+            return 'skip';
+        }
+
+        $message = substr($message, 1);
+        list($command) = explode(' ', $message);
+        $command = mb_strtolower($command);
+        $param = trim(str_replace($command, '', $message));
+
+        $this->history[$userName][] = $message;
+
+        if (!in_array($userName, $this->accessLogins)) {
+            if ($command == self::COMMAND_REG) {
+                if ($this->accessPas == $param) {
+                    $this->addAccess($userName);
+                    foreach ($this->botApi->getUpdates() as $update) {
+                        $message = $update->getMessage() ?: $update->getEditedMessage();
+                        if ($message->getChat()->getUsername() == $userName) {
+                            $this->botApi->deleteMessage(
+                                $message->getChat()->getId(),
+                                $message->getMessageId()
+                            );
+                        }
+                    }
+                    return 'Добро пожаловать!!!';
+                }
+                return 'Неправельный пароль';
+            }
+
+            return 'Не достаточно прав!!!';
+        }
+
+        switch ($command) {
+            case self::COMMAND_STAT:
+                return 'return statistic';
+
+            case self::COMMAND_LIST:
+                $list = 'list commands: '.$this->glue;
+                foreach (self::COMMANDS as $command => $description) {
+                    $list .= '/'.$command.' : '.$description.$this->glue;
+                }
+                return $list;
+
+            case self::COMMAND_ACCESS:
+                return join($this->glue, $this->accessLogins);
+
+            case self::COMMAND_CLI:
+                return shell_exec($param) ?? 'done';
+
+            case self::COMMAND_BOT:
+                return shell_exec('php bin/vasya.php ' . $param) ?? 'done';
+
+            case self::COMMAND_ACCESS_CLEAR:
+                $this->accessLogins = [];
+                return 'До встречи';
+        }
+
+        return 'skip';
+    }
+
+    /**
+     * @param string $userName
+     * @return self
+     */
+    public function addAccess(string $userName): self
+    {
+        if (!in_array($userName, $this->accessLogins)) {
+            $this->accessLogins[] = $userName;
+
+            $this->config->set('telegram.accessUsers', $this->accessLogins);
+            $this->config->save();
+        }
+
+        return $this;
+    }
+}
